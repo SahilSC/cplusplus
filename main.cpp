1f314c3ecd05f4abed8b899b370309ea75b96c9b
Sahil Chowdhury <sahils@Shafins-MacBook-Pro.local>
2023-06-21 22:19:28 -0500

    usacoupdate

--- main.cpp
+++ main.cpp
@@ -1,23 +1,16 @@
 #include <iostream>
 #include <vector>
-#include <string>
-
-void dfstreep(int f);
-
-void dfstreen(int f);
 //#include "Account.h"
 
 using namespace std;
 
-int n;
 vector<vector<int>> adj;
 vector<long> bales;
 vector<vector<long>> ans;
 vector<vector<long>> ans2;
 vector<long> subtree;
 vector<bool> visited;
-vector<bool> visitedt;
-vector<bool> visitedn;
+vector<bool> visiteda;
 
 void dfs(int f){
     visited[f]=true;
@@ -30,32 +23,21 @@ void dfs(int f){
     }
 }
 
-void dfstreep(int f){
-    visitedt[f] = true;
+void move_haybales(int f){
+    visiteda[f] = true;
     for (int o: adj[f]) {
-        if(!visitedt[o]){
-            dfstreep(o);
+        if(!visiteda[o]){
+            move_haybales(o);
             if (subtree[o] > 0) {
                 ans.push_back(vector<long>{o, f, subtree[o]});
-                subtree[o] = 0;
             }
-        }
-    }
-}
-
-void dfstreen(int f){
-    visitedn[f] = true;
-    for (int o: adj[f]) {
-        if(!visitedn[o]){
-            dfstreen(o);
             if (subtree[o] < 0) {
-                ans2.insert(ans2.begin(),(vector<long>{f, o, -subtree[o]}));
-                subtree[o] = 0;
+                ans.insert(ans.begin(),(vector<long>{f, o, -subtree[o]}));
             }
+            subtree[o]=0;
         }
     }
 }
-
 
 int main() {
     ios::sync_with_stdio(0);
@@ -66,8 +48,7 @@ int main() {
     bales.resize(a);
     subtree.resize(a);
     visited.resize(a, false);
-    visitedt.resize(a, false);
-    visitedn.resize(a, false);
+    visiteda.resize(a, false);
     long each = 0;
     for(int i = 0; i < a; i++){
         cin >> bales[i];
@@ -84,8 +65,9 @@ int main() {
         adj[b-1].push_back(c-1);
     }
     dfs(0);
-    dfstreep(0);
-    dfstreen(0);
+//    dfstreep(0);
+//    dfstreen(0);
+    move_haybales(0);
     cout << ans.size() +ans2.size()<< endl;
     for (vector<long> t: ans) {
         for (int i = 0; i < t.size() - 1; i++) {
